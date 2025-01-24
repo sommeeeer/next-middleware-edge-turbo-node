acf9b4e46678201fa5082eb16ee1718d75909c89
magnus <magnus@dahleide.com>
2025-01-24 15:26:10 +0100

    rm fileread

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import { hi } from '@/lib/node-native';
-
 export default async function Home() {
-  const small = await hi();
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      {small}
+      reproduction
     </div>
   );
 }
